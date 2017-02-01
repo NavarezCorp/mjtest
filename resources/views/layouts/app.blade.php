ae0425635b519b44c9f8a304141ecdb05802cc1c
pnavarez <philipnavarez@yahoo.com>
2017-02-01 22:54:28 +0800

    staff profile link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,6 +79,7 @@
                                     <li>
                                         @if (Auth::user()->role == 'staff-1')
                                             <a href="{{ url('/genealogy?sponsor_id=' . Auth::user()->ibo_id . '&placement_id=' . Auth::user()->placement_id) }}">Genealogy</a>
+                                            <a href="{{ url('/user/' . Auth::user()->ibo_id . '/edit') }}">Profile</a>
                                             <a href="{{ url('/productcode') }}" target="_blank">Product Codes</a>
                                             <a href="{{ url('/activationcode') }}" target="_blank">Activation Codes</a>
                                             <a href="{{ url('/ibosearch') }}" target="_blank">IBO Search</a>
