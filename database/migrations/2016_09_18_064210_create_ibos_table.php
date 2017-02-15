c6d137e6e88f027775c779218d637b758b2dc51f
pnavarez <philipnavarez@yahoo.com>
2017-02-15 09:33:16 +0800

    ibo table activation code unique

--- database/migrations/2016_09_18_064210_create_ibos_table.php
+++ database/migrations/2016_09_18_064210_create_ibos_table.php
@@ -25,7 +25,7 @@ class CreateIbosTable extends Migration
             $table->decimal('total_purchase_amount', 9, 2)->nullable();
             $table->integer('ranking_lions_id')->nullable()->index();
             $table->boolean('is_admin')->default(false);
-            $table->string('activation_code', 255)->nullable();
+            $table->string('activation_code', 255)->nullable()->unique();
             $table->string('activation_code_type', 255)->nullable();
             $table->string('email', 255)->nullable();
             $table->integer('gender_id')->index();
