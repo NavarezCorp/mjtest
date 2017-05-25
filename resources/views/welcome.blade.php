4e14900df0c4cb17d401706280c7dafb88fc15d1
pnavarez <navarezphilip@gmail.com>
2017-05-25 22:53:30 +0800

    welcome page new layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway';
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="/css/all.css" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/aboutus') }}">About Us</a>
-                    <a href="{{ url('/contactus') }}">Contact Us</a>
-                    <a href="{{ url('/login') }}">Login</a>
-                    <!--<a href="{{ url('/register') }}">Register</a>-->
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand navbar-link" href="/"> <img src="images/sfi.jpg" width="50"></a>
+                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    {{-- config('app.name', 'Laravel') --}}
-                    <div>
-                        <img src="/images/sfi.jpg" style="width:750px; margin-bottom:20px;">
-                    </div>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active" role="presentation"><a href="{{ url('/aboutus') }}">About Us</a></li>
+                        <li role="presentation"><a href="{{ url('/contactus') }}">Contact Us</a></li>
+                        <li role="presentation"><a href="{{ url('/login') }}">Login</a></li>
+                    </ul>
                 </div>
-                <!--
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-                -->
             </div>
+        </nav>
+        <div class="container">
+            <div id="logo"><img class="img-responsive" src="images/sfi.jpg"></div>
         </div>
+        <script src="/js/vendor.js"></script>
+        <script src="/js/app.js"></script>
     </body>
 </html>
