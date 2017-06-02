ce6ecff2a34ff2641bb38cb4071dc0727b7d581d
pnavarez <greywolf792002@gmail.com.com>
2017-06-03 02:33:21 +0800

    login page new layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,67 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('ibo_id') ? ' has-error' : '' }}">
-                            <label for="ibo_id" class="col-md-4 control-label">IBO ID</label>
-
-                            <div class="col-md-6">
-                                <input id="ibo_id" type="text" class="form-control" name="ibo_id" value="{{ old('ibo_id') }}" required autofocus>
-
-                                @if ($errors->has('ibo_id'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('ibo_id') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+    <div class="login-card">
+        <img src="images/lion3.jpg" class="profile-img-card"/>
+        <p class="profile-name-card"></p>
+        <form class="form-horizontal form-signin" role="form" method="POST" action="{{ url('/login') }}">
+            {{ csrf_field() }}
+            
+            <input class="form-control" type="text" required placeholder="IBO ID" autofocus id="ibo_id" name="ibo_id" value="{{ old('ibo_id') }}"/>
+            @if ($errors->has('ibo_id'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('ibo_id') }}</strong>
+                </span>
+            @endif
+            
+            <input class="form-control" type="password" required placeholder="Password" id="password" name="password"/>
+            @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
+            
+            <div class="checkbox">
+                <div class="checkbox">
+                    <label><input type="checkbox" name="remember"/>Remember me</label>
                 </div>
             </div>
-        </div>
+            
+            <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button>
+        </form>
+        <a href="{{ url('/password/reset') }}" class="forgot-password btn-link">Forgot your password?</a>
     </div>
 </div>
 @endsection
